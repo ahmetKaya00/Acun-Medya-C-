aa9b11dc06c8b6598229ab9c494d42cb0534b57b
Ahmet Kaya <academyakm0@gmail.com>
2025-04-15 21:08:18 +0300

    EfcoreApp Veritabanı İşlemleri

--- FormsApp/obj/Debug/net9.0/FormsApp.AssemblyInfo.cs
+++ FormsApp/obj/Debug/net9.0/FormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2449d016c1c3278862e6ba874d7a08b7ecd692d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215b8dbad3c33d0503dae2c50512e16d8f3d9b2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
